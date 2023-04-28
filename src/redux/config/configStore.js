992f51e4d3bac8825ac005c76196b12024ee7c60
Hira-Yuki <cjsdlf44@gmail.com>
2023-04-28 21:29:41 +0900

    불필요한 파일 제거, Readme 수정

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,11 +1,9 @@
 import { createStore } from "redux";
 import { combineReducers } from "redux";
 import counter from "../modules/counter";
-import users from "../modules/users";
 
 const rootReducer = combineReducers({
     counter,
-    users
 });
 const store = createStore(rootReducer);
 
